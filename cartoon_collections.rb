d332083a7509bcac9f35c0f18269d8cc51179dc3
Patrick Richard <prichard191@gmail.com>
2019-12-06 15:02:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |call| call.length > 4 }
+def long_planeteer_calls(captplanet_calls)
+  captplanet_calls.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese(potentially_cheesy_items)
